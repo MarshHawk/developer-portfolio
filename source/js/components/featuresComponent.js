9f49dfee926f3b2ed839d2e783c9f3e0f0098513
Sean Glover <sglover@ybp.com>
2018-12-24 17:20:59 -0500

    straggling files for features section

---
+++ source/js/components/featuresComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FeatureContainer from '../containers/featureContainer';
+
+let FeaturesComponent = () => (
+    <section id="features">
+		<div className="container">
+			<div className="row">
+				<div className="col-3 col-6-medium col-12-small">
+                    <FeatureContainer />
+                </div>
+            </div>
+        </div>
+    </section>
+)
+
+export default FeaturesComponent
